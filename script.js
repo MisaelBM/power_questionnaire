5cbc3d0aa7e534aedd03ae3ba6eaa704bd113aaa
Misael Morgado <112130147+MisaelBM@users.noreply.github.com>
2024-04-26 06:46:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
+//Variaveis iniciais
 const ambienceAudio = new Audio("Electro Light Loop.mp3");
 ambienceAudio.loop = true;
 ambienceAudio.volume = 0.1;
 const startAudio = new Audio("biodynamic-impact-braam-tonal-dark-184276.mp3");
 startAudio.volume = 0.4;
 const gameAudio = new Audio("Syn Cole - Feel Good (Instrumental) [NCS Purple Remake](MP3_320K).mp3");
-gameAudio.volume = 0.4;
+gameAudio.volume = 0.2;
 gameAudio.loop = true;
+const doublePointsAudio = new Audio("rising-funny-game-effect-132474.mp3");
+doublePointsAudio.volume = 0.6;
+const triplePointsAudio = new Audio("cute-level-up-3-189853.mp3");
+triplePointsAudio.volume = 0.6;
+const youLoseAudio = new Audio("verloren-89595.mp3");
+youLoseAudio.volume = 0.6;
+//Sair do aviso inicial
 document.getElementById("alertButton").addEventListener('click', () => {
     ambienceAudio.play();
     document.getElementById("alertContent").style.display = "none";
     document.getElementById("mainHome").style.display = "flex";
 });
+//Colocar nome do player
 document.getElementById("playButton").addEventListener('click', () => document.getElementById("fixedContent").style.display = "flex");
-document.getElementById('playButtonConfirm').addEventListener("click", () => {
+document.getElementById("playButtonConfirm").addEventListener("click", ConfirmPlay);
+document.getElementById("fixedContent").addEventListener('keyup', e => e.key == "Enter" ? ConfirmPlay() : false);
+function ConfirmPlay() {
     let namePlayer = document.querySelector("#namePlayer").value;
     if (namePlayer) {
+        document.querySelector("#namePlayer").value = "";
         ambienceAudio.pause();
         startAudio.play();
         document.getElementById("namePlayerSpan").innerHTML = namePlayer;
         document.getElementById("mainHome").style.display = "none";
+        document.getElementById("fixedContent").style.display = "none";
         document.getElementById("getReadyContent").style.display = "flex";
         setTimeout(
             function () {
@@ -26,40 +39,92 @@ document.getElementById('playButtonConfirm').addEventListener("click", () => {
                 document.getElementById("mainGame").style.display = "flex";
                 StartGame();
             }
-        , 5000);
+        ,5000);
     };
-});
+};
+//Variaveis iniciais do jogo
 let punctuation = 0;
 let randomPercentage;
 let systemResponse;
 let userResponse;
 let secondsCounter;
-let hearts = 2;
+let hearts;
 let typeLevel;
+document.getElementById("mainGame").addEventListener("keyup", e => {
+    if (e.key == "Enter") {
+        userResponse = document.querySelector("#responseGameInput").value;
+        userResponse == systemResponse ? NextLevel() : LoseHeart();
+    };
+});
+//Funcao que inicia o jogo
 function StartGame() {
+    document.getElementById("responseGameInput").focus();
     gameAudio.play();
     document.querySelector("#responseGameInput").value = '';
     punctuation = 0;
+    hearts = 2;
     GenerateNumber();
-    CounterGame();
+    if (typeLevel > 1) {
+        setTimeout(
+            function () {
+                CounterGame();
+            }
+        ,1500);
+    } else {
+        CounterGame();
+    };
 };
+//Funcao que passa para proxima conta
 function NextLevel() {
     punctuation += typeLevel;
+    document.getElementById("responseGameInput").focus();
     document.querySelector("#responseGameInput").value = '';
     clearInterval(counterTimer);
     GenerateNumber();
-    CounterGame();
-    PunctuationVisor();
+    if (typeLevel > 1 && typeLevel < 5) {
+        setTimeout(
+            function () {
+                CounterGame();
+                PunctuationVisor();
+                console.log("time")
+            }
+        ,1500);
+    } else if (typeLevel < 5) {
+        CounterGame();
+        PunctuationVisor();
+    };
 };
+function RunTime() {
+    
+};
+function CounterRunTime() {
+
+};
+function LoseHeartRunTime() {
+
+};
+function NextLevelRunTime() {
+
+};
+//Funcao que gera uma conta aleatoria
 function GenerateNumber() {
     randomPercentage = parseInt(Math.random() * 100);
+//Run Time    
     if (false) {
-        
+        typeLevel = 5;
+//Triple points        
     } else if (randomPercentage > 4 && randomPercentage <= 14) {
         secondsCounter = 40;
         document.getElementById("counterGame").innerText = secondsCounter;
         typeLevel = 3;
-        let tripleQuest = parseInt(Math.random() * (100 - 36) + 100);
+        document.getElementById("triplePoints").style.display = "flex";
+        triplePointsAudio.play();
+        setTimeout(
+            function () {
+                document.getElementById("triplePoints").style.display = "none";
+            }
+        ,1500);
+        let tripleQuest = parseInt(Math.random() * (100 - 36) + 36);
         let power = 2;
         systemResponse = tripleQuest ** power;
         document.getElementById("questGame").innerHTML = `
@@ -72,11 +137,19 @@ function GenerateNumber() {
                 </div>
             </div>
         `;
+//Double points        
     } else if (randomPercentage > 14 && randomPercentage <= 44) {
         secondsCounter = 35;
         document.getElementById("counterGame").innerText = secondsCounter;
         typeLevel = 2;
-        let doubleQuest = parseInt(Math.random() * (36 - 13) + 36);
+        document.getElementById("doublePoints").style.display = "flex";
+        doublePointsAudio.play();
+        setTimeout(
+            function () {
+                document.getElementById("doublePoints").style.display = "none";
+            }
+        ,1500);
+        let doubleQuest = parseInt(Math.random() * (36 - 13) + 13);
         let power = 2;
         systemResponse = doubleQuest ** power;
         document.getElementById("questGame").innerHTML = `
@@ -89,6 +162,7 @@ function GenerateNumber() {
                 </div>
             </div>
         `;
+//Normal points
     } else {
         secondsCounter = 30;
         document.getElementById("counterGame").innerText = secondsCounter;
@@ -127,6 +201,7 @@ document.getElementById("confirmButton").addEventListener('click', () => {
     userResponse = document.querySelector("#responseGameInput").value;
     userResponse == systemResponse ? NextLevel() : LoseHeart();
 });
+//Funcao do cronometro
 let counterTimer;
 function CounterGame() {
     counterTimer = setInterval(
@@ -137,6 +212,7 @@ function CounterGame() {
         }
     ,1000);
 };
+//Funcao de perder coracao
 function LoseHeart() {
     hearts--;
     typeLevel = 0;
@@ -149,11 +225,25 @@ function LoseHeart() {
     };
     hearts == 0 ? finishGame() : NextLevel();
 };
+//Funcao que mostra pontos na tela
 function PunctuationVisor() {
     document.getElementById("punctuationGame").innerHTML = punctuation;
 };
+//Funcao que finaliza o jogo
 function finishGame() {
     clearInterval(counterTimer);
-    document.getElementById("alertContent").style.display = "flex";
-    document.getElementById("mainGame").style.display = "none";
+    gameAudio.pause();
+    youLoseAudio.play();
+    document.getElementById("youLose").style.display = "flex";
+    setTimeout(
+        function () {
+            document.getElementById("youLose").style.display = "none";
+            document.getElementById("alertContent").style.display = "flex";
+            document.getElementById("mainGame").style.display = "none";
+            document.getElementById("heart2").style.color = "red";
+            document.getElementById("heart1").style.color = "red";
+            document.getElementById("heart2").style.removeProperty("animation");
+            document.getElementById("heart1").style.removeProperty("animation");
+        }
+    ,4000);
 };
